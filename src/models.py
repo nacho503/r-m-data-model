b686d87d8b49461652a773b1ba71c352ae1e085f
nacho503 <ignacio.rosenberg.503@gmail.com>
2022-06-09 00:51:06 +0000

    modelo casi final

--- src/models.py
+++ src/models.py
@@ -8,23 +8,41 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__='user' #Orlando no lo usa porque genera errores
+    id = Column(Integer, primary_key=True) #relacion uno a muchos con user_id
+    name = Column(String(250))
+    password = Column(String(250))
+    favorites=relationship('Favorite')
+    
+
+class Favorite(Base):
+    __tablename__='favorite'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    user_id = Column(Integer, ForeignKey('user.id')) 
+    episode = Column(String(250), ForeignKey('episode.name'))
+    character = Column(String(250), ForeignKey('character.name'))
+
+class Episode(Base):
+    __tablename__='episode'
+    id = Column(Integer, primary_key=True)
+    episode = Column(String(250))
+    name = Column(String(250))
+    character = Column(String(250))
+    air_date=Column(String(250)) 
+    favorite = relationship('Favorite', back_populates='episode') 
+
+class Character(Base):
+    __tablename__='character'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250))
+    status = Column(String(250))
+    episode = Column(String(250))
+    gender=Column(String(250))  
+    species=Column(String(250))  
+    location=Column(String(250))
+    favorite = relationship('Favorite', back_populates='character')    
 
     def to_dict(self):
         return {}
@@ -32,7 +50,7 @@ class Address(Base):
 ## Draw from SQLAlchemy base
 try:
     result = render_er(Base, 'diagram.png')
-    print("Success! Check the diagram.png file")
+    print('Success! Check the diagram.png file')
 except Exception as e:
-    print("There was a problem genering the diagram")
+    print('There was a problem genering the diagram')
     raise e
