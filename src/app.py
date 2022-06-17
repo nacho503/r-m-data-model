629877ca094329cbf6fb281d973421df65972031
nacho503 <ignacio.rosenberg.503@gmail.com>
2022-06-17 00:33:50 +0000

    minuto 40

--- src/app.py
+++ src/app.py
@@ -1,5 +1,22 @@
-from flask import Flask
+from flask import Flask,jsonify,request
 
 app=Flask(__name__)
+
+@app.route('/',methods=['GET'])
+def home():
+    return 'Home page'
+
+@app.route('/characters',methods=['GET']) #recibe todos los personajes
+def get_character():
+    return jsonify(characters) 
+
+
+@app.route('/set_favorite_char',methods=['PUT']) #PENDIENTE ingresa character a favoritos
+def set_favorite_char():
+    #todo = request.json.get("todo")
+    #todos.append(todo)
+    return jsonify(characters) 
+
+
 if __name__ == "__main__":
     app.run(host="localhost",port="5000")
